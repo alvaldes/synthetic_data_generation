25a69f59b90d5dee37e51ccc278d491eabf7a39a
alvaldes <angelluis2605@gmail.com>
2026-06-18 13:41:46 -0600

    chore: bump version to 0.4.0

--- src/dataforge/__init__.py
+++ src/dataforge/__init__.py
@@ -2,7 +2,7 @@ from .pipeline import DataForgePipeline
 from .base_step import BaseStep
 from .config import DataForgeSettings, get_settings, load_settings
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "DataForgePipeline",
     "BaseStep",
